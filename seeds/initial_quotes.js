8fb7fbed8127482d7346aa79a4b5f8f337e40953
Sam Crouch <stcrouch17@gmail.com>
2021-07-21 13:18:31 -1000

    broken quotes

--- seeds/initial_quotes.js
+++ seeds/initial_quotes.js
@@ -7,7 +7,8 @@ exports.seed = function(knex) {
         {quote: "Go to sleep! Go to sleep!", hw_id: 1, img_url: "https://pbs.twimg.com/ext_tw_video_thumb/1089924668667191299/pu/img/REtWii2RRXSR5h6W.jpg" },
         {quote: "Prostitution whore!", hw_id: 2, img_url: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Teresa_Giudice_at_New_York_Fashion_Week_2016.jpg"},
         {quote: "So nasty and so rude", hw_id: 3, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/NeNe_Leakes_at_PaleyFest_2013.jpg/1024px-NeNe_Leakes_at_PaleyFest_2013.jpg"},
-        {quote: "Get off my jock", hw_id: 1, img_url: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Bethenny_Frankel_2009.png"}
+        {quote: "Get off my jock", hw_id: 1, img_url: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Bethenny_Frankel_2009.png"},
+        {quote: "She's cuckoo for cocoa puffs", hw_id: 1, img_url: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Bethenny_Frankel_2009.png"}
       ]);
     });
 };
