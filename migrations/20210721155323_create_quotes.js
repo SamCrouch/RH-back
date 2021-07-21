accce6cc298a376b222a71113fab6fa6d7ac6f2a
Sam Crouch <stcrouch17@gmail.com>
2021-07-21 13:29:28 -1000

    words

--- migrations/20210721155323_create_quotes.js
+++ migrations/20210721155323_create_quotes.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('quotes', table => {
-      table.increments('');
+      table.increments();
       table.string('quote').notNullable();
       table.integer('hw_id');
       table.string('img_url');
