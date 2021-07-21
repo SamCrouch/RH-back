3a2b62d0990f29a5eb270df99919709062dbd3db
Sam Crouch <stcrouch17@gmail.com>
2021-07-21 09:07:38 -1000

    new db seeds

--- seeds/initial_quotes.js
+++ seeds/initial_quotes.js
@@ -4,7 +4,8 @@ exports.seed = function(knex) {
   return knex('quotes').del()
     .then(function () {
       return knex('quotes').insert([
-        {quote: "Go to sleep got to sleep!", hw_id: 1},
+        {quote: "Go to sleep! Go to sleep!", hw_id: 1},
+        {quote: "Get off my jock", hw_id: 1},
         {quote: "Prostitution whore!", hw_id: 2},
         {quote: "So nasty and so rude", hw_id: 3}
       ]);
