5cbc978e56ed98700f8da7b35e6c384f97c4b18e
Sam Crouch <stcrouch17@gmail.com>
2021-07-23 07:32:25 -1000

    new seeds

--- seeds/initial_quotes.js
+++ seeds/initial_quotes.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       return knex('quotes').insert([
         {quote: "Go to sleep! Go to sleep!", hw_id: 1, img_url: "https://pbs.twimg.com/ext_tw_video_thumb/1089924668667191299/pu/img/REtWii2RRXSR5h6W.jpg" },
-        {quote: "I am paying attention! Obviously there has to be something else. It's just not name change and arrested. There has to be something else. Are you stripping? You were stripping. Prostitution whore! You were f--king engaged 19 times! You f--king stupid bitch!", hw_id: 2, img_url: "https://www.bravotv.com/sites/bravo/files/styles/scale--1200/public/2019-07/teresa-table-flip.jpg?itok=HE85VKdU"},
+        {quote: "I am paying attention! Obviously there has to be something else. It's just not name change and arrested. There has to be something else. Are you stripping? You were stripping. Prostitution whore! You were f--king engaged 19 times!", hw_id: 2, img_url: "https://www.bravotv.com/sites/bravo/files/styles/scale--1200/public/2019-07/teresa-table-flip.jpg?itok=HE85VKdU"},
         {quote: "So nasty and so rude", hw_id: 3, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/NeNe_Leakes_at_PaleyFest_2013.jpg/1024px-NeNe_Leakes_at_PaleyFest_2013.jpg"},
         {quote: "Get off my jock", hw_id: 1, img_url: "https://www.bravotv.com/sites/bravo/files/styles/media-gallery-computer/public/field_media_items/2015/04/the-real-housewives-of-new-york-city-season-7-taglines-01.jpg?itok=DlEkUH12"},
         {quote: "She's cuckoo for cocoa puffs", hw_id: 1, img_url: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Bethenny_Frankel_2009.png"},
